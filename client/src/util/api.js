03c0ce1840ec95852694df6714a176628023957d
UEHARA Junji <uehaj@jggug.org>
2016-11-30 00:22:47 +0900

    remove jquery dependency

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,15 +1,9 @@
-import $ from 'jquery';
-
 const urlBase = 'http://localhost:8080/'
 
-/**
- * APIs
- */
 export async function getBooks() {
   const url = urlBase + 'book?max=100';
   const resp = await fetch(url, {
-    method: 'GET',
-    contentType: 'text/plain'
+    method: 'GET'
   });
   return Promise.resolve(await resp.json());
 }
@@ -17,75 +11,32 @@ export async function getBooks() {
 export async function getBook(id) {
   const url = urlBase + `book/${id}.json`;
   const resp = await fetch(url, {
-    method: 'GET',
-    contentType: 'application/json'
+    method: 'GET'
   });
   return Promise.resolve(await resp.json());
 }
 
-export function updateBook(id, book, callback, callbackError) {
-  $.ajax({
-    type: 'PUT',
-    url: urlBase + `book/${id}.json`,
-    contentType: 'application/json',
-    dataType: 'json',
-    data: JSON.stringify(book),
-    cache: false,
-    success: (data) => {
-      if (callback !== undefined) {
-        callback(data)
-      }
-    },
-    error: (xhr, status, err) => {
-      console.error(xhr, status, err.toString())
-      if (callbackError !== undefined) {
-        callbackError(err)
-      }
-    }
+export async function updateBook(id, book) {
+  console.log('book=',book);
+  const url = urlBase + `book/${id}.json`;
+  const resp = await fetch(url, {
+    method: 'PUT',
+    body: book
   });
 }
 
-export function createBook(book, callback, callbackError) {
-  console.log(JSON.stringify(book));
-  $.ajax({
-    type: 'POST',
-    url: urlBase + `book.json`,
-    contentType: 'application/json',
-    dataType: 'json',
-    data: JSON.stringify(book),
-    cache: false,
-    success: (data) => {
-      if (callback !== undefined) {
-        callback(data)
-      }
-    },
-    error: (xhr, status, err) => {
-      console.error(xhr, status, err.toString())
-      if (callbackError !== undefined) {
-        callbackError(err)
-      }
-    }
+export async function createBook(book) {
+  const url = urlBase + `book.json`;
+  const resp = await fetch(url, {
+    method: 'POST',
+    body: book
   });
 }
 
-export function deleteBook(id, callback, callbackError) {
-  $.ajax({
-    type: 'DELETE',
-    url: urlBase + `book/${id}.json`,
-    contentType: 'application/json',
-    dataType: 'json',
-    cache: false,
-    success: (data) => {
-      if (callback !== undefined) {
-        callback(data)
-      }
-    },
-    error: (xhr, status, err) => {
-      console.error(xhr, status, err.toString())
-      if (callbackError !== undefined) {
-        callbackError(err)
-      }
-    }
+export async function deleteBook(id, callback, callbackError) {
+  const url = urlBase + `book/${id}.json`;
+  const resp = await fetch(url, {
+    method: 'DELETE'
   });
 }
 
