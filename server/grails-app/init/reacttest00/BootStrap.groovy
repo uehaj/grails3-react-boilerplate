8b1f7f43528513533d2c88be2253d1d7762a55ad
UEHARA Junji <uehaj@jggug.org>
2016-12-19 06:40:15 +0900

    comment in Bootstrp

--- server/grails-app/init/reacttest00/BootStrap.groovy
+++ server/grails-app/init/reacttest00/BootStrap.groovy
@@ -4,12 +4,10 @@ import sample.*
 class BootStrap {
 
     def init = { servletContext ->
-    /*
         (1..100).each {
              new Book(title: 'title'+it, price: 100+it).save()
              new Author(name: 'name'+it, age: 200+it).save()
         }
-        */
     }
     def destroy = {
     }
