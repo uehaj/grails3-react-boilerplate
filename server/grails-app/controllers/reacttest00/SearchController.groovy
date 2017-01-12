5d369cd268eb16ec28bdcce281f9a6198f93cf23
UEHARA Junji <uehaj@jggug.org>
2017-01-12 22:58:12 +0900

    add search API parameters

--- server/grails-app/controllers/reacttest00/SearchController.groovy
+++ server/grails-app/controllers/reacttest00/SearchController.groovy
@@ -1,10 +1,12 @@
 package reacttest00
 
 import grails.core.GrailsApplication
-
 import sample.Book
+import grails.converters.JSON
 
 class SearchController {
+
+    static final SEARCH_PARAMS_KEYS = [ 'sort', 'order', 'max', 'offset', 'distinct' ]
 
     GrailsApplication grailsApplication
 
@@ -12,17 +14,47 @@ class SearchController {
 
     static responseFormats = ['json']
 
-    def index() {
-        def domainClass = grailsApplication.getDomainClass(params.domainClassName)
-        def result = quickSearchService.search(
-          domainClass: Book,
-          searchParams: [:],
-          searchProperties: [id: "id"],
-          tokens: [','],
-          query: params.ids)
+    static Map pickUpSearchParams(grails.web.servlet.mvc.GrailsParameterMap params) {
+      return params.collectEntries { k, v -> (k.toLowerCase() in SEARCH_PARAMS_KEYS) ? [k,v] : [] }
+    }
 
-        if (params.fields) {
-          def fields = params.fields.split(',')
+    static final REQUIRED_PARAMS = ['domainClass', 'query', 'on']
+
+    /**
+     * Search API.
+     *
+     * API Params:
+     *  - domainClass
+     *  - query
+     *  - on
+     *  - results
+     *
+     * Example:
+     * http://localhost:8080/search?domainClass=sample.Book&on=id&query=1,2,7&results=id,price&max=100&offset=1
+     *
+     */
+    def index() {
+        def requiredButNotSupplied = REQUIRED_PARAMS.find {it -> params[it] == null}
+        if (requiredButNotSupplied != null) {
+          render(["message":"Required api params not supplied: "+requiredButNotSupplied, "error":404] as JSON)
+          return
+        }
+        def domainClass = grailsApplication.getDomainClass(params.domainClass)
+        if (domainClass == null) {
+          render(["message":"Domain class not found: "+params.domainClass, "error":404] as JSON)
+          return
+        }
+        def association  = (params.association) ?: params.on
+
+        def result = quickSearchService.search(
+          domainClass: domainClass.clazz,
+          searchParams: pickUpSearchParams(params),
+          searchProperties: [(params.on): association],
+          tokens: [','],
+          query: params.query)
+
+        if (params.results) {
+          def fields = params.results.split(',')
           respond result.collect { elem ->
             fields.collectEntries{ field ->
               return [field, elem[field]]
